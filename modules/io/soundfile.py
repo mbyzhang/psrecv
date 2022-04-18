4cd224ba199a2e78a4bcb450b19b77be2c44f2ee
Peter Zhang <mbyzhang@outlook.com>
2022-04-18 22:13:14 +0100

    io: refactor soundfile module

--- modules/io/soundfile.py
+++ modules/io/soundfile.py
@@ -1,21 +1,27 @@
+from numpy import block
 from . import SoundSource
 import soundfile
 
 class SoundFileSource(SoundSource):
     def __init__(self, filename, block_size=4096):
-        self.__fs = soundfile.info(filename).samplerate
-        self.__blocks = soundfile.blocks(filename, blocksize=block_size)
+        self.__file = soundfile.SoundFile(filename)
+        self.__blocks_size = block_size
     
     @property
     def fs(self):
-        return self.__fs
+        return self.__file.samplerate
     
     @property
     def stream(self):
         return self.__blocks
     
     def __enter__(self):
+        self.__file.seek(0)
+        self.__blocks = self.__file.blocks(blocksize=self.__blocks_size)
         return self
 
     def __exit__(self, type, value, traceback):
-        self.__blocks.close()
+        pass
+
+    def __del__(self):
+        self.__file.close()
