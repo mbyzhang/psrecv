57a0691f69fd795b1639707891ad4c39610f3029
Peter <me@mby.pw>
2022-05-02 02:43:09 +0000

    delay: handle number of delayed samples being zero

--- modules/transformers/delay.py
+++ modules/transformers/delay.py
@@ -8,6 +8,9 @@ class Delay(Transformer):
         self.padding = np.zeros(self.n)
 
     def __call__(self, data: np.ndarray) -> np.ndarray:
+        if self.n == 0:
+            return data
+
         x = np.concatenate((self.padding, data))
         self.padding = x[-self.n:]
         return x[:len(data)]
